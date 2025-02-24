0d8d4d55bde6416db97cd9f9472b88d067b6be4c
Diego <kupamail@gmail.com>
2025-02-24 09:03:35 -0300

    Ajustando estructura de archivos

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Servidor funcionando", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        # Aquí verificamos el token de verificación
+        token = request.args.get('hub.verify_token')
+        if token == 'afudeteam1234':
+            return request.args.get('hub.challenge'), 200
+        else:
+            return "Token de verificación inválido", 403
+    # Manejo de mensajes POST va aquí
